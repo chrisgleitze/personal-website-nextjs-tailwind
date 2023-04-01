fe053c59b93891fab06564c859492e1c78ce0d0c
Christian Gleitze <christian-gleitze@web.de>
2023-04-02 00:23:53 +0200

    changed pics, copyright in footer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,7 +18,7 @@ import logoPlanetaria from '@/images/logos/planetaria.svg'
 import logoStarbucks from '@/images/logos/starbucks.svg'
 import image1 from '@/images/photos/image-1.jpg'
 import image2 from '@/images/photos/image-2.jpg'
-import image3 from '@/images/photos/image-3.jpg'
+import image3 from '@/images/photos/image-3.jpeg'
 import image4 from '@/images/photos/image-4.jpg'
 import image5 from '@/images/photos/image-5.jpg'
 import { formatDate } from '@/lib/formatDate'
@@ -251,44 +251,43 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
+          Christian Gleitze - Frontend Developer & Podcaster
         </title>
         <meta
           name="description"
-          content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I’m Chris, a Frontend Developer and Podcaster based in Frankfurt, Germany."
         />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Hi! I&apos;m Chris.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+          I&apos;m a Frontend Developer based in Frankfurt am Main, Germany. I focus on ReactJS and I&apos;ve worked with nextJS, Tailwind, Bootstrap, WordPress and Webflow.
+          </p>
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+          For my podcast I talk to founders and other interesting people about their stories and insights. In my blog I write about creativity,
+        culture, startups and technology.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com"
+              href="https://twitter.com/chrisgleitze"
               aria-label="Follow on Twitter"
               icon={TwitterIcon}
+              target="_blank"
             />
             <SocialLink
-              href="https://instagram.com"
-              aria-label="Follow on Instagram"
-              icon={InstagramIcon}
-            />
-            <SocialLink
-              href="https://github.com"
+              href="https://github.com/chrisgleitze"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
+              target="_blank"
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://www.linkedin.com/in/chrisgleitze/"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
+              target="_blank"
             />
           </div>
         </div>
