5b2b4e6a481e0aca25f67a16183ef1194fbb8729
Christian Gleitze <christian-gleitze@web.de>
2023-04-17 16:20:47 +0200

    add code for google tag to _app.jsx
    export default still missing

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from 'react'
+import Script from 'next/script' // for Google Tag Script
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
@@ -15,6 +16,50 @@ function usePrevious(value) {
 
   return ref.current
 }
+
+/* <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7F9GFP91BK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7F9GFP91BK');
+</script> */
+
+function MyGoogleTag({ Component, pageProps }) {
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-7F9GFP91BK`}
+      />
+      <Script id="MyGoogleTag" strategy="lazyOnload">
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7F9GFP91BK');
+  `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+/* 
+export default {
+  function App() {
+
+  },
+
+  function MyGoogleTag() {
+
+  },
+
+}
+*/
 
 export default function App({ Component, pageProps, router }) {
   let previousPathname = usePrevious(router.pathname)
