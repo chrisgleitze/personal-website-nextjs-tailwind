0e715f96c461fd5634f1c63c8e31cc880585e3db
Christian Gleitze <87121518+chrisgleitze@users.noreply.github.com>
2024-01-31 12:23:22 +0100

    v1 of Podcast page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -123,6 +123,7 @@ function MobileNavigation(props) {
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
                 <MobileNavItem href="/projects">Projects</MobileNavItem>
                 <MobileNavItem href="/articles">Blog</MobileNavItem>
+                <MobileNavItem href="/podcast">Podcast</MobileNavItem>
                 <MobileNavItem href="/about">About</MobileNavItem>
               </ul>
             </nav>
@@ -162,6 +163,7 @@ function DesktopNavigation(props) {
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/projects">Projects</NavItem>
         <NavItem href="/articles">Blog</NavItem>
+        <NavItem href="/podcast">Podcast</NavItem>
         <NavItem href="/about">About</NavItem>
       </ul>
     </nav>
---
+++ src/pages/podcast.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+export default function Podcast() {
+  return (
+    <>
+      <Head>
+        <title>Podcast - Christian Gleitze</title>
+        <meta name="description" content="XXX" />
+      </Head>
+      <SimpleLayout title="Podcast" intro="XXX"></SimpleLayout>
+    </>
+  )
+}
