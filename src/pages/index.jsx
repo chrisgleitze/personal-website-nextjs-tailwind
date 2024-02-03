c20f05b7cc2816d800a774fa7af1b0b3d239f82c
Christian Gleitze <87121518+chrisgleitze@users.noreply.github.com>
2024-02-03 12:07:58 +0100

    copy edit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,143 +3,138 @@ import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
 
-import {Card} from '@/components/Card'
-import {Container} from '@/components/Container'
-import {
-    GitHubIcon,
-    LinkedInIcon,
-    TwitterIcon,
-} from '@/components/SocialIcons'
+import { Card } from '@/components/Card'
+import { Container } from '@/components/Container'
+import { GitHubIcon, LinkedInIcon, TwitterIcon } from '@/components/SocialIcons'
 import image1 from '@/images/photos/image-1.jpg'
 import image2 from '@/images/photos/image-2.jpg'
 import image3 from '@/images/photos/image-3.jpg'
 import image4 from '@/images/photos/image-4.jpg'
 import image5 from '@/images/photos/image-5.jpg'
-import {formatDate} from '@/lib/formatDate'
-import {generateRssFeed} from '@/lib/generateRssFeed'
-import {getAllArticles} from '@/lib/getAllArticles'
+import { formatDate } from '@/lib/formatDate'
+import { generateRssFeed } from '@/lib/generateRssFeed'
+import { getAllArticles } from '@/lib/getAllArticles'
 
-function Article({article}) {
-    return (
-        <Card as="article">
-            <Card.Title href={`/articles/${article.slug}`}>
-                {article.title}
-            </Card.Title>
-            <Card.Eyebrow as="time" dateTime={article.date} decorate>
-                {formatDate(article.date)}
-            </Card.Eyebrow>
-            <Card.Description>{article.description}</Card.Description>
-            <Card.Cta>Read article</Card.Cta>
-        </Card>
-    )
+function Article({ article }) {
+  return (
+    <Card as="article">
+      <Card.Title href={`/articles/${article.slug}`}>
+        {article.title}
+      </Card.Title>
+      <Card.Eyebrow as="time" dateTime={article.date} decorate>
+        {formatDate(article.date)}
+      </Card.Eyebrow>
+      <Card.Description>{article.description}</Card.Description>
+      <Card.Cta>Read article</Card.Cta>
+    </Card>
+  )
 }
 
-function SocialLink({icon: Icon, ...props}) {
-    return (
-        <Link className="group -m-1 p-1" {...props}>
-            <Icon
-                className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300"/>
-        </Link>
-    )
+function SocialLink({ icon: Icon, ...props }) {
+  return (
+    <Link className="group -m-1 p-1" {...props}>
+      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+    </Link>
+  )
 }
 
 function Photos() {
-    let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
 
-    return (
-        <div className="mt-16 sm:mt-20">
-            <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-                {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
-                    <div
-                        key={image.src}
-                        className={clsx(
-                            'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
-                            rotations[imageIndex % rotations.length]
-                        )}
-                    >
-                        <Image
-                            src={image}
-                            alt=""
-                            sizes="(min-width: 640px) 18rem, 11rem"
-                            className="absolute inset-0 h-full w-full object-cover"
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div className="mt-16 sm:mt-20">
+      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
+          <div
+            key={image.src}
+            className={clsx(
+              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
+              rotations[imageIndex % rotations.length]
+            )}
+          >
+            <Image
+              src={image}
+              alt=""
+              sizes="(min-width: 640px) 18rem, 11rem"
+              className="absolute inset-0 h-full w-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-export default function Home({articles}) {
-    return (
-        <>
-            <Head>
-                <title>Christian Gleitze - Frontend Engineer</title>
-                <meta
-                    name="description"
-                    content="I’m Chris, a Frontend Engineer based in Frankfurt, Germany."
-                />
-            </Head>
-            <Container className="mt-9">
-                <div className="max-w-2xl">
-                    <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                        Hi! I&apos;m Chris,
-                    </h1>
-                    <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-                        a Junior Frontend Engineer at an IT Consultancy in Frankfurt.
-                    </p>
-                    <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-                        In my blog below I write down lessons I learned and thoughts I want to
-                        share. Without the help of any AI tool. Topics include programming, podcasting, work and life in
-                        general.
-                    </p>
-                    <div className="mt-6 flex gap-6">
-                        <SocialLink
-                            href="https://twitter.com/chrisgleitze"
-                            aria-label="Follow on Twitter"
-                            icon={TwitterIcon}
-                            target="_blank"
-                        />
-                        <SocialLink
-                            href="https://github.com/chrisgleitze"
-                            aria-label="Follow on GitHub"
-                            icon={GitHubIcon}
-                            target="_blank"
-                        />
-                        <SocialLink
-                            href="https://www.linkedin.com/in/chrisgleitze"
-                            aria-label="Follow on LinkedIn"
-                            icon={LinkedInIcon}
-                            target="_blank"
-                        />
-                    </div>
-                </div>
-            </Container>
-            <Photos/>
-            <Container className="mt-24 md:mt-28">
-                <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-                    <div className="flex flex-col gap-16">
-                        {articles.map((article) => (
-                            <Article key={article.slug} article={article}/>
-                        ))}
-                    </div>
-                    <div className="space-y-10 lg:pl-16 xl:pl-24"></div>
-                </div>
-            </Container>
-        </>
-    )
+export default function Home({ articles }) {
+  return (
+    <>
+      <Head>
+        <title>Christian Gleitze - Frontend Engineer</title>
+        <meta
+          name="description"
+          content="I’m Chris, a Frontend Engineer based in Frankfurt, Germany."
+        />
+      </Head>
+      <Container className="mt-9">
+        <div className="max-w-2xl">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            Hi! I&apos;m Chris,
+          </h1>
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+            a Junior Frontend Engineer at an IT Consultancy in Frankfurt.
+          </p>
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+            In my blog below I share lessons I learned and thoughts I want to
+            share. Written without the help of any AI tool. Topics include
+            programming, podcasting, work and life in general.
+          </p>
+          <div className="mt-6 flex gap-6">
+            <SocialLink
+              href="https://twitter.com/chrisgleitze"
+              aria-label="Follow on Twitter"
+              icon={TwitterIcon}
+              target="_blank"
+            />
+            <SocialLink
+              href="https://github.com/chrisgleitze"
+              aria-label="Follow on GitHub"
+              icon={GitHubIcon}
+              target="_blank"
+            />
+            <SocialLink
+              href="https://www.linkedin.com/in/chrisgleitze"
+              aria-label="Follow on LinkedIn"
+              icon={LinkedInIcon}
+              target="_blank"
+            />
+          </div>
+        </div>
+      </Container>
+      <Photos />
+      <Container className="mt-24 md:mt-28">
+        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+          <div className="flex flex-col gap-16">
+            {articles.map((article) => (
+              <Article key={article.slug} article={article} />
+            ))}
+          </div>
+          <div className="space-y-10 lg:pl-16 xl:pl-24"></div>
+        </div>
+      </Container>
+    </>
+  )
 }
 
 export async function getStaticProps() {
-    if (process.env.NODE_ENV === 'production') {
-        await generateRssFeed()
-    }
+  if (process.env.NODE_ENV === 'production') {
+    await generateRssFeed()
+  }
 
-    return {
-        props: {
-            articles: (await getAllArticles())
-                .slice(0, 4)
-                .map(({component, ...meta}) => meta),
-        },
-    }
+  return {
+    props: {
+      articles: (await getAllArticles())
+        .slice(0, 4)
+        .map(({ component, ...meta }) => meta),
+    },
+  }
 }
