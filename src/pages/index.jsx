5690bb322d1f187dbff0f4cbc99a699f576956f5
Christian Gleitze <87121518+chrisgleitze@users.noreply.github.com>
2024-02-14 23:08:48 +0100

    remov unused import statements

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,7 +11,6 @@ import image2 from '@/images/photos/image-2.jpg'
 import image3 from '@/images/photos/image-3.jpg'
 import image4 from '@/images/photos/image-4.jpg'
 import image5 from '@/images/photos/image-5.jpg'
-import { formatDate } from '@/lib/formatDate'
 import { generateRssFeed } from '@/lib/generateRssFeed'
 import { getAllArticles } from '@/lib/getAllArticles'
 
