9ef18f31061f6e2b6e4e7174dbb3aa2d4e1b9682
Christian Gleitze <87121518+chrisgleitze@users.noreply.github.com>
2024-08-07 14:19:05 +0200

    new google tag manager code

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import GTM from 'react-gtm-module'
+import Head from 'next/head'
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
@@ -20,27 +20,33 @@ function usePrevious(value) {
 export default function App({ Component, pageProps, router }) {
   let previousPathname = usePrevious(router.pathname)
 
-  useEffect(() => {
-    const tagManagerArgs = {
-      gtmId: 'G-7F9GFP91BK'
-    }
-    GTM.initialize(tagManagerArgs)
-  }, [])
-
   return (
-      <>
-        <div className="fixed inset-0 flex justify-center sm:px-8">
-          <div className="flex w-full max-w-7xl lg:px-8">
-            <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-          </div>
+    <>
+      <Head>
+        {/* Google Tag Manager (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7F9GFP91BK"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7F9GFP91BK');
+          `
+        }} />
+      </Head>
+      <div className="fixed inset-0 flex justify-center sm:px-8">
+        <div className="flex w-full max-w-7xl lg:px-8">
+          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
         </div>
-        <div className="relative">
-          <Header />
-          <main>
-            <Component previousPathname={previousPathname} {...pageProps} />
-          </main>
-          <Footer />
-        </div>
-      </>
+      </div>
+      <div className="relative">
+        <Header />
+        <main>
+          <Component previousPathname={previousPathname} {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </>
   )
 }
+
