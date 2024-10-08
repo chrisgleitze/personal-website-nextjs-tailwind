cbcc547445b301683daa3c3cff607997e7482197
Christian Gleitze <connectingdotscoding@gmail.com>
2024-10-08 12:00:31 +0200

    add more text to Datenschutzerklärung

--- src/pages/datenschutz.jsx
+++ src/pages/datenschutz.jsx
@@ -82,12 +82,93 @@ export default function Datenschutz() {
             <a
               href="http://tools.google.com/dlpage/gaoptout?hl=de"
               class="text-teal-400 underline hover:no-underline"
+              target="_blank" rel="noreferrer noopener"
             >
               http://tools.google.com/dlpage/gaoptout?hl=de
             </a>.
           </p>
           <p className="pb-6 text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-2xl">
             Datenschutzerklärung für die Nutzung von Google Adsense
+          </p>
+          <p className="pb-6">
+            Diese Website benutzt Google AdSense, einen Dienst zum Einbinden von Werbeanzeigen der Google Inc. („Google“). Google AdSense verwendet sog. „Cookies“, Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website ermöglicht. Google AdSense verwendet auch so genannte Web Beacons (unsichtbare Grafiken). Durch diese Web Beacons können Informationen wie der Besucherverkehr auf diesen Seiten ausgewertet werden.Die durch Cookies und Web Beacons erzeugten Informationen über die Benutzung dieser Website (einschließlich Ihrer IP-Adresse) und Auslieferung von Werbeformaten werden an einen Server von Google in den USA übertragen und dort gespeichert. Diese Informationen können von Google an Vertragspartner von Google weiter gegeben werden. Google wird Ihre IP-Adresse jedoch nicht mit anderen von Ihnen gespeicherten Daten zusammenführen. Sie können die Installation der Cookies durch eine entsprechende Einstellung Ihrer Browser Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website voll umfänglich nutzen können. Durch die Nutzung dieser Website erklären Sie sich mit der Bearbeitung der über Sie erhobenen Daten durch Google in der zuvor beschriebenen Art und Weise und zu dem zuvor benannten Zweck einverstanden.
+          </p>
+          <p className="pb-6 text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-2xl">
+            Datenschutzerklärung für die Nutzung von Twitter
+          </p>
+          <p className="pb-6">
+            Auf unseren Seiten sind Funktionen des Dienstes Twitter eingebunden. Diese Funktionen werden angeboten durch die Twitter Inc., Twitter, Inc. 1355 Market St, Suite 900, San Francisco, CA 94103, USA. Durch das Benutzen von Twitter und der Funktion „Re-Tweet“ werden die von Ihnen besuchten Webseiten mit Ihrem Twitter-Account verknüpft und anderen Nutzern bekannt gegeben. Dabei werden auch Daten an Twitter übertragen.Wir weisen darauf hin, dass wir als Anbieter der Seiten keine Kenntnis vom Inhalt der übermittelten Daten sowie deren Nutzung durch Twitter erhalten. Weitere Informationen hierzu finden Sie in der Datenschutzerklärung von Twitter unter {' '}<a href="http://twitter.com/privacy" class="text-teal-400 underline hover:no-underline" target="_blank" rel="noreferrer noopener">http://twitter.com/privacy</a>. Ihre Datenschutzeinstellungen bei Twitter können Sie in den Konto-Einstellungen unter {' '}<a href="http://twitter.com/account/settings" class="text-teal-400 underline hover:no-underline" target="_blank" rel="noreferrer noopener">http://twitter.com/account/settings</a> ändern. Quellen: {' '}<a href="https://href.li/?http://www.google.com/intl/de/analytics/privacyoverview.html" class="text-teal-400 underline hover:no-underline" target="_blank" rel="noreferrer noopener">Google Analytics Bedingungen</a>, {' '}<a href="http://www.e-recht24.de/artikel/datenschutz/6635-datenschutz-rechtliche-risiken-bei-der-nutzung-von-google-analytics-und-googleadsense.html" class="text-teal-400 underline hover:no-underline" target="_blank" rel="noreferrer noopener">Google Adsense Haftungsausschluss</a>, <a href="http://twitter.com/privacy" class="text-teal-400 underline hover:no-underline" target="_blank" rel="noreferrer noopener">Twitter Bedingungen</a>
+          </p>
+          <p className="pb-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            Kontaktmöglichkeit über die Internetseite
+          </p>
+          <p className="pb-6">
+            Die Internetseite christiangleitze.com enthält aufgrund von gesetzlichen Vorschriften Angaben, die eine schnelle elektronische Kontaktaufnahme zu unserem Unternehmen sowie eine unmittelbare Kommunikation mit uns ermöglichen, was ebenfalls eine allgemeine Adresse der sogenannten elektronischen Post (E-Mail-Adresse) umfasst. Sofern eine betroffene Person per E-Mail oder über ein Kontaktformular den Kontakt mit dem für die Verarbeitung Verantwortlichen aufnimmt, werden die von der betroffenen Person übermittelten personenbezogenen Daten automatisch gespeichert. Solche auf freiwilliger Basis von einer betroffenen Person an den für die Verarbeitung Verantwortlichen übermittelten personenbezogenen Daten werden für Zwecke der Bearbeitung oder der Kontaktaufnahme zur betroffenen Person gespeichert. Es erfolgt keine Weitergabe dieser personenbezogenen Daten an Dritte.
+          </p>
+          <p className="pb-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            Routinemäßige Löschung und Sperrung von personenbezogenen Daten
+          </p>
+          <p className="pb-6">
+            Der für die Verarbeitung Verantwortliche verarbeitet und speichert personenbezogene Daten der betroffenen Person nur für den Zeitraum, der zur Erreichung des Speicherungszwecks erforderlich ist oder sofern dies durch den Europäischen Richtlinien- und Verordnungsgeber oder einen anderen Gesetzgeber in Gesetzen oder Vorschriften, welchen der für die Verarbeitung Verantwortliche unterliegt, vorgesehen wurde. Entfällt der Speicherungszweck oder läuft eine vom Europäischen Richtlinien- und Verordnungsgeber oder einem anderen zuständigen Gesetzgeber vorgeschriebene Speicherfrist ab, werden die personenbezogenen Daten routinemäßig und entsprechend den gesetzlichen Vorschriften gesperrt oder gelöscht.
+          </p>
+          <p className="pb-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            Rechte der betroffenen Person
+          </p>
+          <ol className="pb-6">
+            <li><b>1. Recht auf Bestätigung</b></li>
+            <p className="pb-3">Jede betroffene Person hat das vom Europäischen Richtlinien- und Verordnungsgeber eingeräumte Recht, von dem für die Verarbeitung Verantwortlichen eine Bestätigung darüber zu verlangen, ob sie betreffende personenbezogene Daten verarbeitet werden. Möchte eine betroffene Person dieses Bestätigungsrecht in Anspruch nehmen, kann sie sich hierzu jederzeit an unseren Datenschutzbeauftragten oder einen anderen Mitarbeiter des für die Verarbeitung Verantwortlichen wenden.</p>
+            <li><b>2. Recht auf Auskunt</b></li>
+            <p className="pb-3">Jede von der Verarbeitung personenbezogener Daten betroffene Person hat das vom Europäischen Richtlinien- und Verordnungsgeber gewährte Recht, jederzeit von dem für die Verarbeitung Verantwortlichen unentgeltliche Auskunft über die zu seiner Person gespeicherten personenbezogenen Daten und eine Kopie dieser Auskunft zu erhalten. Ferner hat der Europäische Richtlinien- und Verordnungsgeber der betroffenen Person Auskunft über folgende Informationen zugestanden:</p>
+            <p className="pb-3"><ul>
+              <li>- die Verarbeitungszwecke</li>
+              <li>- die Kategorien personenbezogener Daten, die verarbeitet werden</li>
+              <li>- die Empfänger oder Kategorien von Empfängern, gegenüber denen die personenbezogenen Daten offengelegt worden sind oder noch offengelegt werden, insbesondere bei Empfängern in Drittländern oder bei internationalen Organisationen</li>
+              <li>- falls möglich die geplante Dauer, für die die personenbezogenen Daten gespeichert werden, oder, falls dies nicht möglich ist, die Kriterien für die Festlegung dieser Dauer</li>
+              <li>- das Bestehen eines Rechts auf Berichtigung oder Löschung der sie betreffenden personenbezogenen Daten oder auf Einschränkung der Verarbeitung durch den Verantwortlichen oder eines Widerspruchsrechts gegen diese Verarbeitung</li>
+              <li>- das Bestehen eines Beschwerderechts bei einer Aufsichtsbehörde</li>
+              <li>- wenn die personenbezogenen Daten nicht bei der betroffenen Person erhoben werden: Alle verfügbaren Informationen über die Herkunft der Daten</li>
+              <li>- das Bestehen einer automatisierten Entscheidungsfindung einschließlich Profiling gemäß Artikel 22 Abs.1 und 4 DS-GVO und — zumindest in diesen Fällen — aussagekräftige Informationen über die involvierte Logik sowie die Tragweite und die angestrebten Auswirkungen einer derartigen Verarbeitung für die betroffene Person</li>
+            </ul></p>
+            <p className="pb-3">Ferner steht der betroffenen Person ein Auskunftsrecht darüber zu, ob personenbezogene Daten an ein Drittland oder an eine internationale Organisation übermittelt wurden. Sofern dies der Fall ist, so steht der betroffenen Person im Übrigen das Recht zu, Auskunft über die geeigneten Garantien im Zusammenhang mit der Übermittlung zu erhalten. Möchte eine betroffene Person dieses Auskunftsrecht in Anspruch nehmen, kann sie sich hierzu jederzeit an unseren Datenschutzbeauftragten oder einen anderen Mitarbeiter des für die Verarbeitung Verantwortlichen wenden.</p>
+            <li><b>3. Recht auf Berichtigung</b></li>
+            <p className="pb-3"></p>
+            <li><b>4. Recht auf Löschung (Recht auf Vergessen werden)</b></li>
+            <p className="pb-3"></p>
+            <li><b>5. Recht auf Einschränkung der Verarbeitung</b></li>
+            <p className="pb-3"></p>
+            <li><b>6. Recht auf Datenübertragbarkeit</b></li>
+            <p className="pb-3"></p>
+            <li><b>7. Recht auf Widerspruch</b></li>
+            <p className="pb-3"></p>
+            <li><b>8. Automatisierte Entscheidungen im Einzelfall einschießlich Profiling</b></li>
+            <p className="pb-3"></p>
+            <li><b>9. Empfänger</b></li>
+            <p className="pb-3"></p>
+            <li><b>10. Dritter</b></li>
+            <p className="pb-3"></p>
+            <li><b>11. Einwilligung</b></li>
+            <p className="pb-3"></p>
+          </ol>
+          <p className="pb-6">
+            Lorem
+          </p>
+          <p className="pb-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            HL
+          </p>
+          <p className="pb-6">
+            Lorem
+          </p>
+          <p className="pb-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            HL
+          </p>
+          <p className="pb-6">
+            Lorem
+          </p>
+          <p className="pb-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            HL
+          </p>
+          <p className="pb-6">
+            Lorem
           </p>
           <p className="pb-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
             HL
