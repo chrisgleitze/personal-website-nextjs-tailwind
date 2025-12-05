b58b09617359dbf00e6e452d3cfa0a61fe047a7a
Christian Gleitze <connectingdotscoding@gmail.com>
2025-12-05 20:30:55 +0100

    change yt link on frontpage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -59,7 +59,7 @@ function YouTubeEmbed() {
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/6x_e5zbnlRs"
+        src="https://www.youtube.com/embed/2J4ea38vuDM"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
