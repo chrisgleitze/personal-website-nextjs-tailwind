5fc8a76296c2598ca79eda353a1e491f2fb1f4c1
Christian Gleitze <connectingdotscoding@gmail.com>
2026-02-04 23:44:18 +0100

    replace google tag manager code with code from google analytics website

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,11 @@
 import { useEffect, useRef } from 'react'
 import Head from 'next/head'
 import Script from 'next/script'
-import { useRouter } from 'next/router'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
-
-// const GA_ID = 'G-7F9GFP91BK'
 
 function usePrevious(value) {
   let ref = useRef()
@@ -21,23 +18,7 @@ function usePrevious(value) {
 }
 
 export default function App({ Component, pageProps, router }) {
-  const nextRouter = useRouter()
   let previousPathname = usePrevious(router.pathname)
-
-  // Track SPA route changes
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      if (!window.gtag) return
-      window.gtag('config', GA_ID, {
-        page_path: url,
-      })
-    }
-
-    nextRouter.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      nextRouter.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [nextRouter.events])
 
   return (
     <>
@@ -45,25 +26,21 @@ export default function App({ Component, pageProps, router }) {
         <title>Christian Gleitze</title>
       </Head>
 
-      {/* Google Analytics (GA4) */}
+      {/* Google Tag Manager (gtag.js) */}
       <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-7F9GFP91BK`}
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-7F9GFP91BK"
       />
-
       <Script
-        id="ga-init"
-        strategy="afterInteractive"
-      >
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-7F9GFP91BK', {
-            anonymize_ip: true,
-          });
-        `}
-      </Script>
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7F9GFP91BK');
+          `,
+        }}
+      />
 
       {/* Background frame */}
       <div className="fixed inset-0 flex justify-center sm:px-8">
