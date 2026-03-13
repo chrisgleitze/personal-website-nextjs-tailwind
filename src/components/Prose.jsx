2dab94d2980a3a9129ec0d9ce040058b6a7dc1c0
Christian Gleitze <connectingdotscoding@gmail.com>
2026-03-13 23:44:51 +0100

    edit layout of blog articles; looks leaner now

--- src/components/Prose.jsx
+++ src/components/Prose.jsx
@@ -2,6 +2,8 @@ import clsx from 'clsx'
 
 export function Prose({ children, className }) {
   return (
-    <div className={clsx(className, 'prose dark:prose-invert')}>{children}</div>
+    <div className={clsx(className, 'prose dark:prose-invert [&>p]:my-3 [&>h2]:mt-8 [&>h3]:mt-8 [&>h4]:mt-8 [&>ol]:my-3 [&>ul]:my-3 [&_li]:my-1 [&>blockquote]:my-3 [&>pre]:my-3 [&_ol>li>p]:my-1 [&_ul>li>p]:my-1')}>
+      {children}
+    </div>
   )
 }
