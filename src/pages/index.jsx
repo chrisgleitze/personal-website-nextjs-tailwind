4fd5c25eccbd9e1c056a41a338be81c81b73b73d
Christian Gleitze <connectingdotscoding@gmail.com>
2026-03-16 13:40:27 +0100

    straighten pics on frontpage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -30,7 +30,6 @@ function Article({ article }) {
 }
 
 function Photos() {
-  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
@@ -39,7 +38,6 @@ function Photos() {
             key={image.src}
             className={clsx(
               'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
-              rotations[imageIndex % rotations.length],
             )}
           >
             <Image
