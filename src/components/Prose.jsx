28b749ce10c7e382bf6e833ab930632a2948744d
Christian Gleitze <connectingdotscoding@gmail.com>
2026-03-18 23:41:05 +0100

    automated dev commit

--- src/components/Prose.jsx
+++ src/components/Prose.jsx
@@ -2,7 +2,12 @@ import clsx from 'clsx'
 
 export function Prose({ children, className }) {
   return (
-    <div className={clsx(className, 'prose dark:prose-invert [&>p]:my-3 [&>h2]:mt-8 [&>h3]:mt-8 [&>h4]:mt-8 [&>ol]:my-3 [&>ul]:my-3 [&_li]:my-1 [&>blockquote]:my-3 [&>pre]:my-3 [&_ol>li>p]:my-1 [&_ul>li>p]:my-1')}>
+    <div
+      className={clsx(
+        className,
+        'prose dark:prose-invert [&>blockquote]:my-3 [&>h2]:mt-8 [&>h3]:mt-8 [&>h4]:mt-8 [&>ol]:my-3 [&>p]:my-3 [&>pre]:my-3 [&>ul]:my-3 [&_li]:my-2 [&_ol>li>p]:my-1 [&_ul>li>p]:my-1',
+      )}
+    >
       {children}
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -191,8 +191,8 @@ module.exports = {
             paddingLeft: theme('spacing.6'),
           },
           li: {
-            marginTop: theme('spacing.6'),
-            marginBottom: theme('spacing.6'),
+            marginTop: theme('spacing.2'),
+            marginBottom: theme('spacing.2'),
             paddingLeft: theme('spacing[3.5]'),
           },
           'li::marker': {
