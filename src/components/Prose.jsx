5c245b305e7868883d35940208dbbf4b1223fbc0
Christian Gleitze <connectingdotscoding@gmail.com>
2026-03-19 10:54:08 +0100

    edit line spacing in lists

--- src/components/Prose.jsx
+++ src/components/Prose.jsx
@@ -5,7 +5,7 @@ export function Prose({ children, className }) {
     <div
       className={clsx(
         className,
-        'prose dark:prose-invert [&>blockquote]:my-3 [&>h2]:mt-8 [&>h3]:mt-8 [&>h4]:mt-8 [&>ol]:my-3 [&>p]:my-3 [&>pre]:my-3 [&>ul]:my-3 [&_li]:my-2 [&_ol>li>p]:my-1 [&_ul>li>p]:my-1',
+        'prose dark:prose-invert [&>blockquote]:my-3 [&>h2]:mt-8 [&>h3]:mt-8 [&>h4]:mt-8 [&>ol]:my-3 [&>p]:my-3 [&>pre]:my-3 [&>ul]:my-3 [&_li]:my-0.5 [&_ol>li>p]:my-1 [&_ul>li>p]:my-1',
       )}
     >
       {children}
